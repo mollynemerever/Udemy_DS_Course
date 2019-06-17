73426e018fcb661b97ec86c4b2ab2ec45da0937b
github username <github email address>
2019-06-16 18:22:16 -0700

    integer reversed complete

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,17 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  let array = n
+    .toString()
+    .split("")
+    .reverse()
+    .join("");
+  let value = parseInt(array) * Math.sign(n);
+  debugger;
+  return value;
+}
+
+reverseInt(-90);
 
 module.exports = reverseInt;
